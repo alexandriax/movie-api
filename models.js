4a84c4df3d22bacf7563cab76a1bd6f421c295ee
alexandriax <allyhamilton0@gmail.com>
2024-06-08 11:41:23 -0400

    define movie schema /models.js

--- models.js
+++ models.js
@@ -1,1 +1,17 @@
 const mongoose = require('mongoose');
+
+let movieSchema = mongoose.Schema({
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    genre: {
+        name: String,
+        description: String
+    },
+    director: {
+        name: String,
+        bio: String
+    },
+    imagePath: String,
+    featured: Boolean
+});
+
