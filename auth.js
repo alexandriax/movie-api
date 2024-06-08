fecb6f4a5ffc5598c2da2b425b6a8fc80afc6f12
alexandriax <allyhamilton0@gmail.com>
2024-06-08 19:00:15 -0400

    add login endpoint /auth.js

---
+++ auth.js
@@ -0,0 +1,21 @@
+// POST login
+
+module.exports = (router) => {
+    router.post('/login', (req, res) => {
+        passport.authenticate('local', {session: false}, (error, user, info) => {
+            if (error || !user) {
+                return res.status(400).json({
+                    message: 'something is broken',
+                    user: user
+                });
+            }
+            req.login(user, {session: false}, (error) => {
+                if(error) {
+                    res.send(error);
+                }
+                let token = generateJWTToken(user.toJSON());
+                return res.json({ user, token });
+            });
+        })(req, res);
+    });
+}
