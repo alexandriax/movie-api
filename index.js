154b77e424bd720032e4729aa95902f5f1501c23
alexandriax <allyhamilton0@gmail.com>
2024-06-19 23:19:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,10 @@ const Users = models.User;
 }); */
 
 // connects mongoose to database online
-mongoose.connect( 'mongodb+srv://allywally:LDR1358iqoa!!@firstcluster.c8pne3j.mongodb.net/myFlixDB?retryWrites=true&w=majority'); 
+mongoose.connect( 'mongodb+srv://allywally:LDR1358iqoa!!@firstcluster.c8pne3j.mongodb.net/myFlixDB?retryWrites=true&w=majority', 
+    { useNewUrlParser: true, useUnifiedTopology: true
+
+}); 
 
 
 //
